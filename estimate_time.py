616cc7f75e5558288559ccd558fa33609153f4e2
Xiaosu Wang <105274211@student.swin.edu.au>
2025-05-20 09:35:49 +1000

    Delete estimate_time.py

--- estimate_time.py
+++
@@ -1,4 +0,0 @@
-def estimate_travel_time(volume, distance_km, speed_kmph=60, intersection_delay=30):
-    time_base = distance_km / speed_kmph * 3600
-    volume_factor = 1 + (volume / 1000)
-    return time_base * volume_factor + intersection_delay
